a0079228e5113a7696c6886e160ffee1a05281db
shamanthkv553 <shamanthkv123@gmail.com>
2024-03-28 22:32:18 +0530

    babel-bug-fix

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: [
+      '@babel/preset-env', // for compiling modern JavaScript to compatible versions
+      '@babel/preset-react' // for compiling JSX syntax
+    ]
+  };
+  
